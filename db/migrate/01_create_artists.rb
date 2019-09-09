f6e03ce63bb06c59d4c28fd3d42fd06ec00ee4d9
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-09 13:08:42 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,5 +1,5 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
-  def change
+class CreateArtists < ActiveRecord::Migration[5.2]
+   def change
     create_table :artists do |t|
       t.string :name
       t.string :genre
@@ -7,4 +7,15 @@ class CreateArtists < ActiveRecord::Migration[4.2]
       t.string :hometown
     end
   end
-end
+  
+   has_many :tires
+
+  after_create :make_tires
+
+  def make_tires
+    4.times { Tire.create(car: self.id) }
+  end
+    
+
+end 
+
--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -2,4 +2,10 @@ class AddFavoriteFoodToArtists < ActiveRecord::Migration[4.2]
   def change
     add_column :artists, :favorite_food, :string
   end
+  
+  class AddGenderToArtists < ActiveRecord::Migration
+  def change
+    add_column :artists, :gender, :string
+  end
+end 
 end
