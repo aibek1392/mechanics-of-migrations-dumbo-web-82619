08d4b5e1bbdf00260b5aaafa65cf5e1656f63fbb
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-09 13:13:34 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,5 +1,10 @@
-class Artist < ActiveRecord::Base
-def change create_table :artists do |t|
-  t.string :name
+class CreateArtists < ActiveRecord::Migration[4.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
 end
